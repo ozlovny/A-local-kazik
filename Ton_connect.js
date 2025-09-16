f66fe2d12f4dce5cc69e3464d86b4ae52bd562aa
ozlovny <olegsmirnov12220@gmail.com>
2025-09-16 14:31:45 +0300

    Update Ton_connect.js

--- Ton_connect.js
+++ Ton_connect.js
@@ -1,3 +1,10 @@
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+      manifestUrl: 'https://phenomenal-bubblegum-d6b71e.netlify.app/tonconnect-manifest.json',
+      buttonRootId: 'ton-connect-button'
+    });
+
+    const recipientAddress = "EQDYXMQCS9pzb_9oFiJjV97si_kNZwh0TU-0UtmFG-bHlqGf";
+
 async function checkPendingTopUp() {
   const pending = parseFloat(localStorage.getItem('pendingTopUp'));
   if (!pending || pending < 0.01) return;
